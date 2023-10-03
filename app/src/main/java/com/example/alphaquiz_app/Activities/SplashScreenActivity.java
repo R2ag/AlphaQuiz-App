a4ab39daa0bf5403ca3077938295865f4d74dff1
Rafael Lacerda <rlag@outlook.com>
2023-10-02 21:24:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/alphaquiz_app/Activities/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.alphaquiz_app.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.alphaquiz_app.MainActivity;
+import com.example.alphaquiz_app.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
