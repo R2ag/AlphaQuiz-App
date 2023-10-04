bcf6d40fe74df9fd4e3bf9556da3527df7c1fb68
Rafael <rlag@outlook.com>
2023-10-04 08:58:02 -0300

    Alteração da Pagina Inicial

---
+++ app/src/main/java/com/example/alphaquiz_app/Models/SetModel.java
@@ -0,0 +1,17 @@
+package com.example.alphaquiz_app.Models;
+
+public class SetModel {
+    String setName;
+
+    public SetModel(String setName) {
+        this.setName = setName;
+    }
+
+    public String getSetName() {
+        return setName;
+    }
+
+    public void setSetName(String setName) {
+        this.setName = setName;
+    }
+}
